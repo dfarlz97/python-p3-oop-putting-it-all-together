6791430084fe9867f84d77e0dd628ac6d07b5d26
+ <+>
2023-05-08 22:00:59 -0400

    satisfy deliverables

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, author="", page_count=0, genre=""):
+        self.title = title
+        self.author = author
+        self.page_count = page_count
+        self.genre = genre
+    
+ 
+    def set_page_count(self,page_count):
+        if type(page_count) == int:
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    
+    def get_page_count(self):
+        return self._page_count
+    
+
+    page_count = property(get_page_count, set_page_count)
